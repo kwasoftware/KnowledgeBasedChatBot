100273ba8bad62ca29401d4618c876fae6542ac3
Aarnav Kapoor <kapaarnav@gmail.com>
2024-08-05 10:38:05 -0400

    pysqlite3-commit-4

--- src/set_retriever.py
+++ src/set_retriever.py
@@ -1,13 +1,12 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import chromadb
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import streamlit as st
 import upload_file
-
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import sqlite3
 
 data = upload_file.get_doc()
 
