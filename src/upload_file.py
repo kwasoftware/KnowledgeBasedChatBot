07743238b695fb3b3da7130af3db25a23c465a41
Aarnav Kapoor <kapaarnav@gmail.com>
2024-08-05 12:00:01 -0400

    upload-file-change-8

--- src/upload_file.py
+++ src/upload_file.py
@@ -5,10 +5,13 @@ st.title("Knowledge Bot")
 
 #Uploads the file from the pdf
 def get_doc():
-    uploaded_file = st.file_uploader(label="Choose a file", 
+    uploaded_file = st.sidebar.file_uploader(label="Choose a file", 
                                      accept_multiple_files=False, 
                                      type=['pdf'])
-    if uploaded_file is not None:
-        loader = PyMuPDFLoader(uploaded_file.name)
-        data = loader.load()
-        return data
+    while uploaded_file is None:
+        uploaded_file = st.sidebar.file_uploader(label="Choose a file", 
+                                     accept_multiple_files=False, 
+                                     type=['pdf'])
+    loader = PyMuPDFLoader(uploaded_file.name)
+    data = loader.load()
+    return data
