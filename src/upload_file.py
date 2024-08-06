3df7c1aca69522cfd6fbcde3b8f71f8919eb216d
Aarnav Kapoor <kapaarnav@gmail.com>
2024-08-06 10:43:59 -0400

    upload-file-change-12

--- src/upload_file.py
+++ src/upload_file.py
@@ -5,11 +5,6 @@ st.title("Knowledge Bot")
 
 #Uploads the file from the pdf
 def get_doc(uploaded_file):
-    loader = PyMuPDFLoader(uploaded_file)
-    data = loader.load()
-    return data
-
-def send_data():
     uploaded_file = st.sidebar.file_uploader("Upload a PDF", type="pdf")
     if uploaded_file is not None:
 
@@ -18,7 +13,8 @@ def send_data():
             f.write(uploaded_file.getbuffer())
 
         # Load the document using PyMuPDFLoader
-        data = get_doc("temp.pdf")
+        loader = PyMuPDFLoader("temp.pdf")
+        data = loader.load()
         st.write("File successfully loaded!")
         return data
     else:
