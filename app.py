18689b06469b5b2e29387e3b24b636cf6406a4e3
Aarnav Kapoor <kapaarnav@gmail.com>
2024-08-07 11:01:12 -0400

    chroma-error

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
-import chromadb.config
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_core.chat_history import BaseChatMessageHistory
